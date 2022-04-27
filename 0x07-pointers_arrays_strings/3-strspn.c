646f84dc5349687020cd3c4474a2730331ff1e1e
petersonfalsh <pafalase@gmail.com>
2022-04-27 11:51:50 -0700

    edited spaces in task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,9 +4,9 @@
  * _strspn - Locates a character in a string
  * @s: This is the main C string to be scanned.
  * @accept: This is the string containing the list of characters to match in s
-
  * Return: return count
  **/
+
 unsigned int _strspn(char *s, char *accept)
 
 {
